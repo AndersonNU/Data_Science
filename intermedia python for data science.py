e7cfd8501fb4d37feeef20ae76565e88894b989e
Anderson <taoruipeng2017@u.northwestern.edu>
2017-08-23 16:57:04 -0500

    begin the course at datacamp for python

---
+++ intermedia python for data science.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  9 15:21:09 2017
+
+@author: anderson
+"""
+# Print the last item from year and pop
+year = [1992,1003]
+print(year[-1])
+print(pop[-1])
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year, pop)
+
+# Display the plot with plt.show()
+plt.show()
+
+# Change the line plot below to a scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Put the x-axis on a logarithmic scale
+plt.xscale('log')
+
+# Show plot
+plt.show()
+
+# Histogram
+# Build histogram with 5 bins
+plt.hist(life_exp, bins= 5)
+
+# Show and clean up plot
+plt.show()
+plt.clf()
+
+# Build histogram with 20 bins
+plt.hist(life_exp, bins = 20)
+
+# Show and clean up again
+plt.show()
+plt.clf()
+
+# Basic scatter plot, log scale
+plt.scatter(gdp_cap, life_exp)
+plt.xscale('log') 
+
+# Strings
+xlab = 'GDP per Capita [in USD]'
+ylab = 'Life Expectancy [in years]'
+title = 'World Development in 2007'
+
+# Add axis labels
+plt.xlabel(xlab)
+plt.ylabel(ylab)
+
+
+# Add title
+plt.title(title)
+
+# After customizing, display the plot
+plt.show()
---
+++ test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# import numpy as np
+# a = [3, 4, 5, 1, 2, 5]
+# a = np.asarray(a)
+# index = a.argsort()[:3]
+# print(index)
+a = 3
